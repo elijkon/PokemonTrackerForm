43e63c83f2689a0a36fd792f5d3ad13bd3ec0443
elijkon <timetocleanup20@gmail.com>
2025-09-14 16:13:28 -0500

    reset button finished

--- app/src/main/java/com/example/pokemontrackerform/MainActivity.java
+++ app/src/main/java/com/example/pokemontrackerform/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.pokemontrackerform;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class        MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //spinner
+    Spinner levelSpinner;
+
+    //buttons
+    Button resetButton;
+
+    //editable texts
+    EditText nationalNumber, name, species, height, weight, hp, attack, defense;
+
+    //radio group
+    RadioGroup rGroup;
+
+
+
+    //level select listener
+    AdapterView.OnItemSelectedListener spinnerListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selectedLevelType = parent.getItemAtPosition(position).toString();
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +54,53 @@ public class        MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //populating levels array
+        List<String> levels = new ArrayList<>();
+        levels.add("N/A");
+        for (int i = 1; i <= 50 ; i ++) {
+            levels.add(String.valueOf(i));
+        }
+
+        //getting ids
+        levelSpinner = findViewById(R.id.levelSpinner);
+        resetButton = findViewById(R.id.resetButton);
+        nationalNumber = findViewById(R.id.nationalNumberInput);
+        name = findViewById(R.id.nameInput);
+        species = findViewById(R.id.speciesInput);
+        height = findViewById(R.id.heightInput);
+        weight = findViewById(R.id.weightInput);
+        hp = findViewById(R.id.hpInput);
+        attack = findViewById(R.id.attackInput);
+        defense = findViewById(R.id.defenseInput);
+        rGroup = findViewById(R.id.genderGroup);
+
+        //adapter for levels spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                levels
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        levelSpinner.setAdapter(adapter);
+        levelSpinner.setOnItemSelectedListener(spinnerListener);
+
+        //resetButton
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attack.setText(getString(R.string.defaultBaseStats));
+                nationalNumber.setText(getString(R.string._896));
+                name.setText(getString(R.string.glastrier));
+                species.setText(getString(R.string.wild_horse_pokemon));
+                height.setText(getString(R.string.defaultMeters));
+                weight.setText(getString(R.string.defaultKg));
+                hp.setText(getString(R.string.defaultBaseStats));
+                defense.setText(getString(R.string.defaultBaseStats));
+                rGroup.clearCheck();
+                levelSpinner.setSelection(0);
+            }
         });
     }
 }
